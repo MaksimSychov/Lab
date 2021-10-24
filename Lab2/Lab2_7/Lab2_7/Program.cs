ec44d01e2c21e89037488a39732f4d6ca9f7dd01
MaksimSychov <m.sychov-ex3eme@mail.ru>
2021-10-24 16:11:34 +0300

    Lab2

---
+++ Lab2/Lab2_7/Lab2_7/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x > 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("Ответ: I четверть");
+    }
+    else if (y < 0)
+    {
+        Console.WriteLine("Ответ: IV четверть");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("Ответ: 0");
+    }
+}
+else if (x < 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("Ответ: II четверть");
+    }
+    else if (y < 0)
+    {
+        Console.WriteLine("Ответ: III четверть");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("Ответ: 0");
+    }
+}
+else if (x == 0) 
+{
+    Console.WriteLine("Ответ: 0");
+}
+Console.ReadKey();
