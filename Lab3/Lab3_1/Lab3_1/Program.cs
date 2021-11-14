07ab813ced9507a3335e282fa8c5a48709c55ede
MaksimSychov <m.sychov-ex3eme@mail.ru>
2021-11-14 19:38:57 +0300

    Lab3

--- Lab3/Lab3_1/Lab3_1/Program.cs
+++ Lab3/Lab3_1/Lab3_1/Program.cs
@@ -10,24 +10,24 @@ class Program
         Random random = new Random();
         Console.Write("Введите число элементов массива: ");
         int k = int.Parse(Console.ReadLine());
-        int[] list = new int[k];
-        for (int i = 0; i < list.Length; i++)
+        int[] array = new int[k];
+        for (int i = 0; i < array.Length; i++)
         {
-            list[i] = random.Next(-30, 45);
+            array[i] = random.Next(-30, 45);
         }
         Console.WriteLine("Вывод массива:");
-        for (int i = 0; i < list.Length; i++)
+        for (int i = 0; i < array.Length; i++)
         {
-            Console.Write(list[i] + " ");
+            Console.Write(array[i] + " ");
         }
         Console.WriteLine();
         Console.WriteLine("Элементы массива в обратном направлении без отрицательных чисел: ");
-        list = list.Reverse().ToArray();
-        for (int i = 0, count = 0; i < list.Length; i++)
+        array = array.Reverse().ToArray();
+        for (int i = 0, count = 0; i < array.Length; i++)
         {
-            if (list[i] >= 0)
+            if (array[i] >= 0)
             {
-                Console.Write(list[i] + " ");
+                Console.Write(array[i] + " ");
                 count++;
             }   
         }
